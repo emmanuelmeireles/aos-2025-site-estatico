b3108cea5134cff9c9be06c0cb012b009f4fb7dc
Emmanuel Meireles Silva <emmanuelmeireles@gmail.com>
2025-03-17 11:58:43 +0000

    primeira tentativa

--- src/main/java/aos/exibidor_sites_estaticos_01/ExibidorSitesEstaticos01Application.java
+++ src/main/java/aos/exibidor_sites_estaticos_01/ExibidorSitesEstaticos01Application.java
@@ -1,13 +1,15 @@
-package com.example.demo;
+package aos.exibidor_sites_estaticos_01;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class ExibidorSitesEstaticos01Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(ExibidorSitesEstaticos01Application.class, args);
+
+		
 	}
 
 }
--- src/test/java/aos/exibidor_sites_estaticos_01/ExibidorSitesEstaticos01ApplicationTests.java
+++ src/test/java/aos/exibidor_sites_estaticos_01/ExibidorSitesEstaticos01ApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package aos.exibidor_sites_estaticos_01;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class ExibidorSitesEstaticos01ApplicationTests {
 
 	@Test
 	void contextLoads() {
